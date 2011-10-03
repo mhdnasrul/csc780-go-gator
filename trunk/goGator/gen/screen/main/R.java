0c39f114d9a28b78b23b485afb3a2b88987f8cb6
itsnilay <itsnilay@85276dc7-3539-7209-74ce-976d98e5471d>
2011-10-03 07:35:18 +0000

    Maps Tab configured according to Design.

--- trunk/goGator/gen/screen/main/R.java
+++ trunk/goGator/gen/screen/main/R.java
@@ -19,21 +19,17 @@ public final class R {
         public static final int icon_default=0x7f020005;
     }
     public static final class id {
-        public static final int button=0x7f050007;
-        public static final int first_tab=0x7f050001;
-        public static final int fourth_tab=0x7f050009;
-        public static final int mapview=0x7f05000a;
-        public static final int mycafe=0x7f050003;
-        public static final int mycampus=0x7f050002;
-        public static final int myride=0x7f050005;
-        public static final int myvisit=0x7f050004;
-        public static final int second_tab=0x7f050006;
-        public static final int tab_host=0x7f050000;
-        public static final int third_tab=0x7f050008;
+        public static final int first_tab=0x7f050000;
+        public static final int mapview=0x7f050005;
+        public static final int mycafe=0x7f050002;
+        public static final int mycampus=0x7f050001;
+        public static final int myride=0x7f050004;
+        public static final int myvisit=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int mainmap=0x7f030001;
+        public static final int hometab=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int maptab=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
