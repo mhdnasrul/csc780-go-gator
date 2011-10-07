da01729a8ea5caa87dd4dfc2e93711a652757190
nikita8ad@gmail.com <nikita8ad@gmail.com@85276dc7-3539-7209-74ce-976d98e5471d>
2011-10-07 03:33:45 +0000

    New icons added

--- goGator/gen/screen/main/R.java
+++ goGator/gen/screen/main/R.java
@@ -11,25 +11,33 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_dialog_dialer=0x7f020000;
-        public static final int ic_dialog_email=0x7f020001;
-        public static final int ic_dialog_info=0x7f020002;
-        public static final int ic_dialog_map=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int icon_default=0x7f020005;
+        public static final int ic_camera=0x7f020000;
+        public static final int ic_dialog_dialer=0x7f020001;
+        public static final int ic_dialog_email=0x7f020002;
+        public static final int ic_dialog_info=0x7f020003;
+        public static final int ic_dialog_map=0x7f020004;
+        public static final int ic_home=0x7f020005;
+        public static final int ic_info=0x7f020006;
+        public static final int ic_map=0x7f020007;
+        public static final int icon=0x7f020008;
+        public static final int icon_default=0x7f020009;
     }
     public static final class id {
-        public static final int first_tab=0x7f050000;
-        public static final int mapview=0x7f050005;
-        public static final int mycafe=0x7f050002;
-        public static final int mycampus=0x7f050001;
-        public static final int myride=0x7f050004;
-        public static final int myvisit=0x7f050003;
+        public static final int buildinglist=0x7f050000;
+        public static final int button=0x7f050001;
+        public static final int first_tab=0x7f050002;
+        public static final int mapview=0x7f050007;
+        public static final int mycafe=0x7f050004;
+        public static final int mycampus=0x7f050003;
+        public static final int myride=0x7f050006;
+        public static final int myvisit=0x7f050005;
     }
     public static final class layout {
-        public static final int hometab=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int maptab=0x7f030002;
+        public static final int building_list=0x7f030000;
+        public static final int desc=0x7f030001;
+        public static final int hometab=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int maptab=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
