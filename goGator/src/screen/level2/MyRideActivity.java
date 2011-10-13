ba661aa43449eaa7f5d08bc1e82fa90311bad884
itsnilay <itsnilay@85276dc7-3539-7209-74ce-976d98e5471d>
2011-10-13 06:42:01 +0000

    Code for walking distance between 2 geo locations done, where first is Current geo location.

--- goGator/src/screen/level2/MyRideActivity.java
+++ goGator/src/screen/level2/MyRideActivity.java
@@ -20,7 +20,17 @@
 
 package screen.level2;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import screen.main.R;
 import com.google.android.maps.GeoPoint;
@@ -32,6 +42,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
@@ -102,11 +113,20 @@ public class MyRideActivity extends com.google.android.maps.MapActivity {
                     .getLongitude()*1000000));
                    mc.animateTo(p);
 
+	
+//	           double src_lat = 25.04202; // the testing source 
+//	           double src_long = 121.534761; 
+//	           double dest_lat = 25.05202; // the testing destination 
+//	           double dest_long = 121.554761;
+	       
+	           double dest_lat =  37.722329; // the testing destination 
+	           double dest_long = -122.478322; 
+//	           GeoPoint srcGeoPoint = new GeoPoint((int) (src_lat * 1E6), (int) (src_long * 1E6)); 
+	          
+	           GeoPoint destGeoPoint = new GeoPoint((int) (dest_lat * 1E6), 
+	           (int) (dest_long * 1E6)); 
 
-
-
-
-
+	           DrawPath(p, destGeoPoint, Color.GREEN, mapView); 
 
         }
 
@@ -176,4 +196,77 @@ public class MyRideActivity extends com.google.android.maps.MapActivity {
         protected boolean isRouteDisplayed() {
             return false;
         }
+	private void DrawPath(GeoPoint src,GeoPoint dest, int color, MapView mMapView01) 
+	{ 
+	// connect to map web service 
+	StringBuilder urlString = new StringBuilder(); 
+	urlString.append("http://maps.google.com/maps?f=d&hl=en"); 
+	urlString.append("&saddr=");//from 
+	urlString.append( Double.toString((double)src.getLatitudeE6()/1.0E6 )); 
+	urlString.append(","); 
+	urlString.append( Double.toString((double)src.getLongitudeE6()/1.0E6 )); 
+	urlString.append("&daddr=");//to 
+	urlString.append( Double.toString((double)dest.getLatitudeE6()/1.0E6 )); 
+	urlString.append(","); 
+	urlString.append( Double.toString((double)dest.getLongitudeE6()/1.0E6 )); 
+	urlString.append("&dirflg=w&ie=UTF8&0&om=0&output=kml"); 
+//	Log.d("xxx","URL="+urlString.toString()); 
+	// get the kml (XML) doc. And parse it to get the coordinates(direction route). 
+	Document doc = null; 
+	HttpURLConnection urlConnection= null; 
+	URL url = null; 
+	try 
+	{ 
+	url = new URL(urlString.toString()); 
+	urlConnection=(HttpURLConnection)url.openConnection(); 
+	urlConnection.setRequestMethod("GET"); 
+	urlConnection.setDoOutput(true); 
+	urlConnection.setDoInput(true); 
+	urlConnection.connect(); 
+
+	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance(); 
+	DocumentBuilder db = dbf.newDocumentBuilder(); 
+	doc = db.parse(urlConnection.getInputStream()); 
+
+	if(doc.getElementsByTagName("GeometryCollection").getLength()>0) 
+	{ 
+	//String path = doc.getElementsByTagName("GeometryCollection").item(0).getFirstChild().getFirstChild().getNodeName(); 
+	String path = doc.getElementsByTagName("GeometryCollection").item(0).getFirstChild().getFirstChild().getFirstChild().getNodeValue() ; 
+//	Log.d("xxx","path="+ path); 
+	String [] pairs = path.split(" "); 
+	String[] lngLat = pairs[0].split(","); // lngLat[0]=longitude lngLat[1]=latitude lngLat[2]=height 
+	// src 
+	GeoPoint startGP = new GeoPoint((int)(Double.parseDouble(lngLat[1])*1E6),(int)(Double.parseDouble(lngLat[0])*1E6)); 
+	mMapView01.getOverlays().add(new rideOverlay(startGP,startGP,1)); 
+	GeoPoint gp1; 
+	GeoPoint gp2 = startGP; 
+	for(int i=1;i<pairs.length;i++) // the last one would be crash 
+	{ 
+	lngLat = pairs[i].split(","); 
+	gp1 = gp2; 
+	// watch out! For GeoPoint, first:latitude, second:longitude 
+	gp2 = new GeoPoint((int)(Double.parseDouble(lngLat[1])*1E6),(int)(Double.parseDouble(lngLat[0])*1E6)); 
+	mMapView01.getOverlays().add(new rideOverlay(gp1,gp2,2,color)); 
+//	Log.d("xxx","pair:" + pairs[i]); 
+	} 
+	mMapView01.getOverlays().add(new rideOverlay(dest,dest, 3)); // use the default color 
+	} 
+	} 
+	catch (MalformedURLException e) 
+	{ 
+	e.printStackTrace(); 
+	} 
+	catch (IOException e) 
+	{ 
+	e.printStackTrace(); 
+	} 
+	catch (ParserConfigurationException e) 
+	{ 
+	e.printStackTrace(); 
+	} 
+	catch (SAXException e) 
+	{ 
+	e.printStackTrace(); 
+	} 
+	}
 }
