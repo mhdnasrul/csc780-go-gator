1fbe2a0ab64b3d0e17ec1d3093af7eb020f36615
nikita8ad@gmail.com <nikita8ad@gmail.com@85276dc7-3539-7209-74ce-976d98e5471d>
2011-10-14 02:57:08 +0000

    Modified OnClickListener to TextView and added Building and department icons

--- goGator/src/screen/level1/MapsActivity.java
+++ goGator/src/screen/level1/MapsActivity.java
@@ -42,17 +42,15 @@ public class MapsActivity extends MapActivity {
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         
-        
-        
         List<Overlay> mapOverlays = mapView.getOverlays();
         Drawable drawable = this.getResources().getDrawable(R.drawable.flag);
         mapOverlay itemizedoverlay = new mapOverlay(drawable, this);
         	
-        GeoPoint ADMpoint = new GeoPoint(37721140, -122476450);
+        GeoPoint ADMpoint = new GeoPoint(37721274, -122476868);
         OverlayItem overlayitem = new OverlayItem(ADMpoint, "Administration Building", null);
         itemizedoverlay.addOverlay(overlayitem);
         
-        GeoPoint Libpoint = new GeoPoint(37721350, -122478190);
+        GeoPoint Libpoint = new GeoPoint(37721435, -122477453);
         OverlayItem overlayitem2 = new OverlayItem(Libpoint, "SFSU Library!", null);
         itemizedoverlay.addOverlay(overlayitem2);
         
@@ -60,47 +58,47 @@ public class MapsActivity extends MapActivity {
         OverlayItem overlayitem3 = new OverlayItem(Mcpoint, "McKenna Theatre", null);
         itemizedoverlay.addOverlay(overlayitem3);
         
-        GeoPoint CCpoint = new GeoPoint(37722280, -122478890);
+        GeoPoint CCpoint = new GeoPoint(37722021, -122478550);
         OverlayItem overlayitem4 = new OverlayItem(CCpoint, "Ceasar Chavez", null);
         itemizedoverlay.addOverlay(overlayitem4);
         
-        GeoPoint BUSpoint = new GeoPoint(37722030, -122476770);
+        GeoPoint BUSpoint = new GeoPoint(37722091, -122476868);
         OverlayItem overlayitem5 = new OverlayItem(BUSpoint, "Business", null);
         itemizedoverlay.addOverlay(overlayitem5);
         
-        GeoPoint HSSpoint = new GeoPoint(37721960, -122476040);
+        GeoPoint HSSpoint = new GeoPoint(37721626, -122476584);
         OverlayItem overlayitem6 = new OverlayItem(HSSpoint, "HSS", null);
         itemizedoverlay.addOverlay(overlayitem6);
         
-        GeoPoint HUMpoint = new GeoPoint(37722520, -122480930);
+        GeoPoint HUMpoint = new GeoPoint(37722180, -122480910);
         OverlayItem overlayitem7 = new OverlayItem(HUMpoint, "Humanities", null);
         itemizedoverlay.addOverlay(overlayitem7);
         
-        GeoPoint Vilpoint = new GeoPoint(37722990, -122480790);
+        GeoPoint Vilpoint = new GeoPoint(37722991, -122480561);
         OverlayItem overlayitem8 = new OverlayItem(Vilpoint, "The Village", null);
         itemizedoverlay.addOverlay(overlayitem8);
         
-        GeoPoint SSpoint = new GeoPoint(37723430, -122480630);
+        GeoPoint SSpoint = new GeoPoint(37723353, -122480532);
         OverlayItem overlayitem9 = new OverlayItem(SSpoint, "Student Services", null);
         itemizedoverlay.addOverlay(overlayitem9);
         
-        GeoPoint THpoint = new GeoPoint(37724080, -122476910);
+        GeoPoint THpoint = new GeoPoint(37724070, -122476876);
         OverlayItem overlayitem10 = new OverlayItem(THpoint, "Thornton Hall", null);
         itemizedoverlay.addOverlay(overlayitem10);
         
-        GeoPoint HHpoint = new GeoPoint(37723510, -122476440);
+        GeoPoint HHpoint = new GeoPoint(377235210, -122475809);
         OverlayItem overlayitem11 = new OverlayItem(HHpoint, "Hensill Hall", null);
         itemizedoverlay.addOverlay(overlayitem11);
         
-        GeoPoint SCIpoint = new GeoPoint(37722940, -122476100);
+        GeoPoint SCIpoint = new GeoPoint(37722863, -122476262);
         OverlayItem overlayitem12 = new OverlayItem(SCIpoint, "Science Building", null);
         itemizedoverlay.addOverlay(overlayitem12);
         
-        GeoPoint Burkpoint = new GeoPoint(37722780, -122479130);
+        GeoPoint Burkpoint = new GeoPoint(37722734, -122478966);
         OverlayItem overlayitem13 = new OverlayItem(Burkpoint, "Burk Hall", null);
         itemizedoverlay.addOverlay(overlayitem13);
         
-        GeoPoint FApoint = new GeoPoint(37722490, -122478010);
+        GeoPoint FApoint = new GeoPoint(37722541, -122479776);
         OverlayItem overlayitem14 = new OverlayItem(FApoint, "Fine Arts", null);
         itemizedoverlay.addOverlay(overlayitem14);
                 
