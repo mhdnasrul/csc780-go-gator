99e8986ffe83e602e71749c36529a56048baf472
nikita8ad@gmail.com <nikita8ad@gmail.com@85276dc7-3539-7209-74ce-976d98e5471d>
2011-11-21 06:38:15 +0000

    Basic DB attached for MyCafe

--- goGator/src/screen/level2/MyCafeActivity.java
+++ goGator/src/screen/level2/MyCafeActivity.java
@@ -1,32 +1,73 @@
 package screen.level2;
 
-import main.data.CafeItems;
-import main.data.DeptItems;
+import gatorDB.gatorDBhelper;
+
+import java.util.ArrayList;
+
 import main.overlay.MyOverlayItem;
-import android.app.AlertDialog;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
-import android.widget.ListView;
-import android.widget.TextView;
-import screen.level1.MoreActivity;
+
 import screen.level3.MyCampusDetailActivity;
 import screen.main.R;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
- 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 public class MyCafeActivity extends ListActivity {
+
+    private ArrayList<String> results = new ArrayList<String>();
+	private String tableName = gatorDBhelper.tableName;
+	private SQLiteDatabase newDB;
  
      public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<MyOverlayItem>(this, R.layout.building_list, CafeItems.getCafeItems()));
+		openAndQueryDatabase();
+        displayResultList();
      }
+	 
+	 private void displayResultList() {
+		       
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.building_list, results));
+        getListView().setTextFilterEnabled(true);
+		
+	}
+	
+		private void openAndQueryDatabase() {
+		try {
+			gatorDBhelper dbHelper = new gatorDBhelper(this.getApplicationContext());
+			newDB = dbHelper.getWritableDatabase();
+			Cursor c = newDB.rawQuery("SELECT Name FROM " +
+	    			tableName +
+	    			" where Type = 'Cafe'", null);
+
+	    	if (c != null ) {
+	    		if  (c.moveToFirst()) {
+	    			do {
+	    				String Name = c.getString(c.getColumnIndex("Name"));
+	    				results.add(Name);
+	    			}while (c.moveToNext());
+	    		} 
+	    	}			
+		} catch (SQLiteException se ) {
+        	Log.e(getClass().getSimpleName(), "Could not create or Open the database");
+        } finally {
+        	if (newDB != null) 
+        		newDB.execSQL("DELETE FROM " + tableName);
+        		newDB.close();
+        }
+
+	}
+
+		public void onListItemClick(){}
+	
     	@Override
         public void onListItemClick( ListView parent, View v, int position, long id)  {
            Intent intent = new Intent(MyCafeActivity.this, MyCampusDetailActivity.class);
