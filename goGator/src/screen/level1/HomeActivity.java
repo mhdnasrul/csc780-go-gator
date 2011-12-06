36543140dbff2edfd8b8a0a0f36d9855da5355f6
itsnilay <itsnilay@85276dc7-3539-7209-74ce-976d98e5471d>
2011-12-06 03:24:52 +0000

    Minor fixes..

--- goGator/src/screen/level1/HomeActivity.java
+++ goGator/src/screen/level1/HomeActivity.java
@@ -20,28 +20,12 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
-public class HomeActivity extends Activity implements SensorEventListener {
+public class HomeActivity extends Activity  {
 	
-	private SensorManager mSensorManager;
-	private Sensor accelerometer;
-	private Sensor magnetometer;
-	private float[] mGravity;
-	private float[] mGeomagnetic;
-	private float azimut;
-	private Canvas canvas;
-	
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hometab);
-        
-     // Register the sensor listeners
-     		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-     		accelerometer = mSensorManager
-     				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-     		magnetometer = mSensorManager
-     				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-     		canvas = new Canvas();
     }
   
     public void MyCampus(View view){
@@ -63,59 +47,6 @@ public class HomeActivity extends Activity implements SensorEventListener {
 		Intent intent = new Intent(this, MyRideActivity.class);
 		startActivity(intent);	
 	}
-    
-    protected void onResume() {
-		super.onResume();
-		// Re-catch Magnetometer on Resume
-		mSensorManager.registerListener(this, accelerometer,
-				SensorManager.SENSOR_DELAY_UI);
-		mSensorManager.registerListener(this, magnetometer,
-				SensorManager.SENSOR_DELAY_UI);
-	}
 
-	protected void onPause() {
-		super.onPause();
-		// Unregister Sensor
-		mSensorManager.unregisterListener(this);
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-			mGravity = event.values;
-		if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-			mGeomagnetic = event.values;
-		if (mGravity != null && mGeomagnetic != null) {
-			float R[] = new float[9];
-			float I[] = new float[9];
-			boolean success = SensorManager.getRotationMatrix(R, I, mGravity,
-					mGeomagnetic);
-			if (success) {
-				float orientation[] = new float[3];
-				SensorManager.getOrientation(R, orientation);
-				azimut = orientation[0]; // orientation contains: azimut, pitch
-											// and roll
-				azimut = azimut * 360 / (2 * 3.14159f);
-			}
-		}
-		
-//		Bitmap bMap = BitmapFactory.decodeResource(getResources(),
-//				 R.drawable.wheel);
-//				 Matrix mat = new Matrix();
-//				 mat.postTranslate(-25, -25);
-//				 if (azimut >= 0)
-//					 mat.postRotate(azimut, bMap.getWidth()/2,bMap.getHeight()/2);
-//				 else
-//					 mat.postRotate(360+azimut, bMap.getWidth()/2,bMap.getHeight()/2);
-//				 mat.postTranslate(25, 25);
-//				// Bitmap bMapRotate = Bitmap.createBitmap(bMap, 0, 0,
-//				 //bMap.getWidth(), bMap.getHeight(), mat, true);
-//				 canvas.drawBitmap(bMap, mat, new Paint());
-	}
       
 }
